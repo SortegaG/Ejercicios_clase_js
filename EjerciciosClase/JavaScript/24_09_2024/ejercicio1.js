155183e2d8decc95a97fc9e8bfbc55e92a8b2266
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-09-24 16:28:20 +0200

    Ejercicio terminado

--- EjerciciosClase/JavaScript/24_09_2024/ejercicio1.js
+++ EjerciciosClase/JavaScript/24_09_2024/ejercicio1.js
@@ -1,1 +1,14 @@
 //Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
+
+function wordcount(word,text) {  // Función que recoge dos parámetros
+    let arrText = text.toUpperCase().split('');  // Transformamos un string en un array y lo ponemos en mayúsculas
+    let arrWord = word.toUpperCase(); // Transformamos la letra en mayúscula
+    let count = 0;
+    for (let i = 0; i < arrText.length; i++) { // Bucle for que recorre todos los elementos del array arrText
+        if (arrText[i] === arrWord) {
+            count++; //Incrementamos el valor de la variable count en 1
+        }
+    } return count;
+}
+
+console.log(wordcount("e", "Esta patata esta muy caliente y quema mucho las manos cuando la coges"))
