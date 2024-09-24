01a625be2e93a3d9092e79a24680dff817fede38
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-09-24 16:41:55 +0200

    Ejercicio terminado

--- EjerciciosClase/JavaScript/24_09_2024/ejercicio1.js
+++ EjerciciosClase/JavaScript/24_09_2024/ejercicio1.js
@@ -1,10 +1,15 @@
 //Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase
 
 function wordcount(word,text) {  // Función que recoge dos parámetros
-    let arrText = text.toUpperCase().split('');  // Transformamos un string en un array y lo ponemos en mayúsculas
-    let arrWord = word.toUpperCase(); // Transformamos la letra en mayúscula
+    // Transformamos un string en un array y lo ponemos en mayúsculas
+    let arrText = text.toUpperCase().split('');  
+    // Transformamos la letra en mayúscula
+    let arrWord = word.toUpperCase(); 
+    // Creamos una variable que lleve la cuenta y comienza en 0
     let count = 0;
-    for (let i = 0; i < arrText.length; i++) { // Bucle for que recorre todos los elementos del array arrText
+    // Bucle for que comienza con un valor de 0, si ese número es menor que la longitud de array arrText, incrementa en 1 su valor
+    for (let i = 0; i < arrText.length; i++) {
+        // Condición: Si el valor de la posicion i de arrText es estrictamente igual que el valor de arrWord
         if (arrText[i] === arrWord) {
             count++; //Incrementamos el valor de la variable count en 1
         }
--- EjerciciosClase/JavaScript/24_09_2024/ejercicio2.js
+++ EjerciciosClase/JavaScript/24_09_2024/ejercicio2.js
@@ -1,10 +1,16 @@
 // Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
 
 function imprimeImpares(number) {
-    let numeroIncrementado = number + 50;
-    let arrNumeros = [];
+    // Creamos una variable que sume 50 a nuestro parametro number
+    let numeroIncrementado = number + 50; 
+    // Creamos una variable que es un array vacío
+    let arrNumeros = []; 
+    /* Bucle for que comienza con el valor de number, se efectúa siempre que sea menor que numeroIncrementado,
+    y le suma 1 si se cumplen las condiciones*/
     for (let i = number; i < numeroIncrementado; i++ ) {
+        // Condición : [i] = valor del array en la posición i. Si el resto de dividir [i] entre 2 no es cero
         if ([i] % 2 !== 0) {
+            // Introducimos el valor de i en el array arrNumeros
             arrNumeros.push(i);
         }
     } return arrNumeros;
